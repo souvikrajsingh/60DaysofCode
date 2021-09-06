e14bc5c842fcf815c75e79c3746374476aeac687
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-09-06 09:41:28 +0530

    finished Merge_thenSORT

--- Day13/Merge_thenSORT.java
+++ Day13/Merge_thenSORT.java
@@ -9,9 +9,9 @@ public class Merge_thenSORT {
         System.out.println("Enter the number of elements in first array : ");
         int n = sc.nextInt();
         System.out.println("Enter the number of elements in the secon array : ");
-        int n1= sc.nextInt();
-        int[] arr = new int[n+n1];
-        int[] arr2 =new int[n1];
+        int n1 = sc.nextInt();
+        int[] arr = new int[n + n1];
+        int[] arr2 = new int[n1];
         System.out.println("Enter elements in the array 1 : ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -20,16 +20,44 @@ public class Merge_thenSORT {
         for (int i = 0; i < arr2.length; i++) {
             arr2[i] = sc.nextInt();
         }
-        mergethenSort(arr,arr2, n , n1);
-        System.out.println("after merging : " + Arrays.toString(arr) );
+        mergethenSort(arr, arr2, n, n1);
+        System.out.println("after merging : " + Arrays.toString(arr));
         /*System.out.println("after merging : " );
         for (int i = 0; i < arr.length; i++) {
 
             System.out.print(arr[i] + " ");
-        }*/
 
+        }*/
+        insertionsort(arr);
+        System.out.println("Sorted Array: "  + Arrays.toString(arr));
     }
 
+        static void insertionsort(int [] arr)
+        {
+            for(int i=0;i<arr.length-1;i++)
+            {
+                for(int j=i+1; j > 0 ; j--)
+                {
+                    if(arr[j]<arr[j-1])
+                    {
+                        swap(arr , j ,j-1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    private static void swap(int[] arr, int j, int i) {
+        int temp;
+
+        temp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=temp;
+
+    }
     private static void mergethenSort(int[] arr, int[] arr2, int size1 ,int size2) {
         for (int i = 0; i < size1;  i++) {
             for (int j = 0; j < size2; j++) {
