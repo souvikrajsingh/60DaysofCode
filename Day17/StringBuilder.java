1e2ea52cb04bae7f9995ed8a2cc7db9ac70aeced
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-09-15 16:11:31 +0530

    day17

---
+++ Day17/StringBuilder.java
@@ -0,0 +1,15 @@
+package com.souvik.Day17;
+
+public class StringBuilder{
+    public static void main(String[] args) {
+        String name="Souvik Raj Singh";
+
+//        System.out.println(name);
+
+        String a = "Souvik";
+        System.out.println(a);
+
+        a="raj Singh";
+        System.out.println(a);
+    }
+}
